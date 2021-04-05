73e8dbf9151d57c72d6bbe572349326ba04c88d7
rikp777 <rikpeeters11@hotmail.com>
2021-04-05 10:19:28 +0200

    PDR Update

--- gateway/src/main/java/flowcontrol/gateway/GatewayApplication.java
+++ gateway/src/main/java/flowcontrol/gateway/GatewayApplication.java
@@ -3,8 +3,15 @@ package flowcontrol.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+//import org.springframework.web.reactive.function.client.WebClient;
 
+//@EnableEurekaClient
 @SpringBootApplication
 @EntityScan(basePackageClasses = {
         GatewayApplication.class,
@@ -16,4 +23,14 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+//        return builder.routes().build();
+//    }
+//
+//    @Bean
+//    @LoadBalanced
+//    public WebClient.Builder loadBalancedWebClientBuilder() {
+//        return WebClient.builder();
+//    }
 }
