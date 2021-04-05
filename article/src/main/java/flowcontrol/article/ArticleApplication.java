927a3eece8e1fda267f4a6ce4a53d1a7c2aeb925
rikp777 <rikpeeters11@hotmail.com>
2021-04-05 11:44:11 +0200

    Github actions workflow database fix?

--- article/src/main/java/flowcontrol/article/ArticleApplication.java
+++ article/src/main/java/flowcontrol/article/ArticleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ArticleApplication {
-    //test change github actions workflow
+    //test change github actions workfl
     public static void main(String[] args) {
         SpringApplication.run(ArticleApplication.class, args);
     }
